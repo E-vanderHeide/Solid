2d113bf7fea7ca425f974b09974f7aa92957ec4b
E-vanderHeide <Ewoud.vanderheide@outlook.com>
2016-02-24 18:06:21 +0100

    More bubbles!

--- public/app/components/bubbles/bubbleController.js
+++ public/app/components/bubbles/bubbleController.js
@@ -2,95 +2,76 @@ var diameter = 800,
 format = d3.format(",d"),
 color = d3.scale.category20c();
 
-var bubble = d3.layout.pack()
-.sort(null)
-.size([diameter, diameter])
-.padding(1.5);
+var maxCircleDiameter = 150;
 
 var svg = d3.select("#canvas-bubbles").append("svg")
-.attr("width", diameter)
-.attr("height", diameter)
-.attr("class", "bubble");
+  .attr("width", diameter)
+  .attr("height", diameter)
+  .attr("class", "bubble");
 
-d3.json("assets/Data/flare.json", function(error, root) {
-if (error) throw error;
+  d3.json("assets/Data/flare.json", function(data) {
+    var valuesForCountry =  getDataForCountry("Spain",data );
+    $.each(valuesForCountry.data, function(index, value){
+      var enter = svg.selectAll("circle").data(valuesForCountry.data).enter();
+      enter.append("circle")
+          .attr("cy", 60)
+          .attr("cx", function(d, i) { return i * 100 + 30; })
+          .attr("r", function(d) 
+            {
+              //size based on percentage
+              return maxCircleDiameter * (d.D/valuesForCountry.getTotalAmountOfDeaths());
+            });
+      enter.append("circle")
+          .attr("cy", 60)
+          .style("stroke", "red")
+          .style("stroke-width", "3")
+          .attr("cx", function(d, i) { return i * 100 + 30; })
+          .attr("r", function(d) 
+            {
+              //size based on percentage
+              return maxCircleDiameter * (d.M/valuesForCountry.getTotalAmountOfMedia()); 
+            });
+  });
+});
 
-var data = getDataForCountry("Spain", root);
-      // var node = svg.selectAll(".node")
-      //     .data(bubble.nodes(classes(root))
-      //     .filter(function(d) { return !d.children; }))
-      //   .enter().append("g")
-      //     .attr("class", "node")
-      //     .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
 
-      // node.append("title")
-      //     .text(function(d) { return d.className + ": " + format(d.value); });
 
-      // node.append("circle")
-      //     .attr("r", function(d) { return d.r; })
-      //     .style("fill", function(d) { return color(d.packageName); });
-
-      // node.append("text")
-      //     .attr("dy", ".3em")
-      //     .style("text-anchor", "middle")
-      //     .text(function(d) { return d.className.substring(0, d.r / 3); });
-    });
-
-    // Returns a flattened hierarchy containing all leaf nodes under the root.
-    function classes(root) {
-      var classes = [];
-
-      function recurse(name, node) {
-        if (node.children) node.children.forEach(function(child) { recurse(node.name, child); });
-        else classes.push({packageName: name, className: node.name, value: node.size});
-      }
-
-      recurse(null, root);
-      return {children: classes};
-    }
-
-    d3.select(self.frameElement).style("height", diameter + "px");
-
-    function getDataForCountry(country, json)
+    function getDataForCountry(country, data)
     {
       var countryValues = {
-        Deaths:{
-          D01:0,
-          D02:0,
-          D03:0,
-          D04:0,
-          D05:0,
-          D06:0,
-          D07:0
+        name :"",
+        data:[],
+        getTotalAmountOfDeaths : function(){
+
+          var total = 0;
+          
+          $.each(this.data, function(index, value){
+            
+            total += value.D;
+          });
+          return total;
         },
-        Media:{
-          M01:0,
-          M02:0,
-          M03:0,
-          M04:0,
-          M05:0,
-          M06:0,
-          M07:0
+         getTotalAmountOfMedia : function(){
+
+          var total = 0;
+          
+          $.each(this.data, function(index, value){
+            
+            total += value.M;
+          });
+          return total;
         }
       };
-
-      $.each(json, function(index, value){
+      $.each(data, function(index, value){
         if(value.Country == country){
-          countryValues.Deaths.D01 = value.D01;
-          countryValues.Deaths.D02 = value.D02;
-          countryValues.Deaths.D03 = value.D03;
-          countryValues.Deaths.D04 = value.D04;
-          countryValues.Deaths.D05 = value.D05;
-          countryValues.Deaths.D06 = value.D06;
-          countryValues.Deaths.D07 = value.D07;
-
-          countryValues.Media.M01 = value.M01;
-          countryValues.Media.M02 = value.M02;
-          countryValues.Media.M03 = value.M03;
-          countryValues.Media.M04 = value.M04;
-          countryValues.Media.M05 = value.M05;
-          countryValues.Media.M06 = value.M06;
-          countryValues.Media.M07 = value.M07;
+          countryValues.name= country;
+          countryValues.data.push({Cause:"01", D:parseInt(value.D01), M:parseInt(value.M01)});
+          countryValues.data.push({Cause:"02", D:parseInt(value.D02), M:parseInt(value.M02)});
+          countryValues.data.push({Cause:"03", D:parseInt(value.D03), M:parseInt(value.M03)});
+          countryValues.data.push({Cause:"04", D:parseInt(value.D04), M:parseInt(value.M04)});
+          countryValues.data.push({Cause:"05", D:parseInt(value.D05), M:parseInt(value.M05)});
+          countryValues.data.push({Cause:"06", D:parseInt(value.D06), M:parseInt(value.M06)});
+          countryValues.data.push({Cause:"07", D:parseInt(value.D07), M:parseInt(value.M07)});
         }
       });
       return countryValues;
