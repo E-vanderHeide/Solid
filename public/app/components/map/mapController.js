29b615724164953475e15e45fe18c3b2522ad173
E-vanderHeide <Ewoud.vanderheide@outlook.com>
2016-03-04 10:24:36 +0100

    a little more restucture:

--- public/app/components/map/mapController.js
+++ public/app/components/map/mapController.js
@@ -200,46 +200,8 @@ d3.json("assets/Data/mock.json", function(err, data) {
   //action when clicking with the mouse on a country
   .on("click", function(d){
     selectedCountry = getCountryByName(countries, d.properties.name);
-    var circle= bubbleSvg.selectAll("circle");
-    var data = circle.data(selectedCountry.data);
-    var enter = data.enter();
-
-    enter.append("circle")
-    .attr("class", "deathBubble")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d){
-      //size based on percentage
-      return maxCircleDiameter * (d.D/selectedCountry.getTotalAmountOfDeaths());
-    });
-    enter.append("circle")
-    .attr("class", "mediaBubble")
-    .attr("cy", 60)
-    .style("stroke", "red")
-    .style("stroke-width", "3")
-    .style("fill-opacity","0.5")
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) 
-    {
-      //size based on percentage
-      return maxCircleDiameter * (d.M/selectedCountry.getTotalAmountOfMedia()); 
-    });  
-
-
-    var deathBubbles= bubbleSvg.selectAll(".deathBubble");
-
-    deathBubbles.transition().duration(500)
-    .attr("r", function(d){
-     return maxCircleDiameter * (d.D/selectedCountry.getTotalAmountOfDeaths());
-   });
-
-    var mediaBubbles= bubbleSvg.selectAll(".mediaBubble");
-
-    mediaBubbles.transition().duration(500)
-    .attr("r", function(d){
-      return maxCircleDiameter * (d.M/selectedCountry.getTotalAmountOfMedia()); 
-    });
-
+    changeCountrySelection();
+  
 
   });
 
