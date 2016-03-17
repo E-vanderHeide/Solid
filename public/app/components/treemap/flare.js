69dc40a1a73e043759261eea4b25f9709c5490c4
ingridsorensen <iingridsorensen@gmail.com>
2016-03-17 14:31:57 +0100

    coloring

--- public/app/components/treemap/flare.js
+++ public/app/components/treemap/flare.js
@@ -2,7 +2,7 @@ var sweden = {
   "name":"flare",
   "children": 
   [
-    {"name":"Communicatable",
+    {"name":"Communicatable","minCoverage": 0,"maxCoverage": 672500,"totalCoverage":21,"coverage":270590, 
     "children":
     [
     {"name":"Tuberculosis","depth":2,"size":0,"coverage":4370},
@@ -13,6 +13,25 @@ var sweden = {
     {"name":"Upper respiratory infections","depth":2,"size":0,"coverage":52131},
     {"name":"Otitis media","depth":2,"size":0,"coverage":0},
     {"name":"Meningitis","depth":2,"size":0,"coverage":0}
+    ]
+    }
+  ]
+}
+var norway = {
+  "name":"flare",
+  "children": 
+  [
+    {"name":"Communicatable", "minCoverage": 20, "maxCoverage": 93733, "totalCoverage":270590, "coverage":270590, 
+    "children":
+    [
+    {"name":"Tuberculosis","depth":2,"size":0,"coverage":44400},
+    {"name":"HIV/AIDS","depth":2,"size":0,"coverage":61250},
+    {"name":"Diarrheal diseases","depth":2,"size":0,"coverage":4250},
+    {"name":"Intestinal infectious diseases","depth":2,"size":0,"coverage":93733},
+    {"name":"Lower respiratory infections","depth":2,"size":0,"coverage":14776},
+    {"name":"Upper respiratory infections","depth":2,"size":0,"coverage":52131},
+    {"name":"Otitis media","depth":2,"size":0,"coverage":20},
+    {"name":"Meningitis","depth":2,"size":0,"coverage":30}
     ]
     }
   ]
