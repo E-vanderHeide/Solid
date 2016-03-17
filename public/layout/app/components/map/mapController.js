083eda76cde4c218782e4b66fb1b00054b6b3518
ingridsorensen <iingridsorensen@gmail.com>
2016-03-17 17:08:00 +0100

    merging to index layout

---
+++ public/layout/app/components/map/mapController.js
@@ -0,0 +1,245 @@
+var colorCriterium = "D01";
+var mapCountries;
+  var COLOR_COUNTS = 11;
+   var colors = [];
+drawMap();
+var svg;
+function drawMap(){
+  d3.json("assets/Data/mock.json", function(err, data) {
+
+  countries = getDataForCountries(data);  
+});
+
+
+  function Interpolate(start, end, steps, count) {
+    var s = start,
+    e = end,
+    final = s + (((e - s) / steps) * count);
+    return Math.floor(final);
+  }
+
+  function Color(_r, _g, _b) {
+    var r, g, b;
+    var setColors = function(_r, _g, _b) {
+      r = _r;
+      g = _g;
+      b = _b;
+    };
+    
+    setColors(_r, _g, _b);
+    this.getColors = function() {
+      var colors = {
+        r: r,
+        g: g,
+        b: b
+      };
+      return colors;
+    }; 
+  }
+
+  function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+  }
+   var config = {
+  "color0":"#99ccff",
+  "color1":"#0050A1",
+  "width":850,
+  "height":850}
+  
+  var width = config.width,
+  height = config.height;
+
+  var COLOR_FIRST = config.color0, COLOR_LAST = config.color1;
+
+  var rgb = hexToRgb(COLOR_FIRST);
+
+  var COLOR_START = new Color(rgb.r, rgb.g, rgb.b);
+
+  rgb = hexToRgb(COLOR_LAST);
+  var COLOR_END = new Color(rgb.r, rgb.g, rgb.b);
+
+  var startColors = COLOR_START.getColors(),
+  endColors = COLOR_END.getColors();
+
+ 
+  for (var i = 0; i < COLOR_COUNTS; i++) {
+    var r = Interpolate(startColors.r, endColors.r, COLOR_COUNTS, i);
+    var g = Interpolate(startColors.g, endColors.g, COLOR_COUNTS, i);
+    var b = Interpolate(startColors.b, endColors.b, COLOR_COUNTS, i);
+    colors.push(new Color(r, g, b));
+  }
+  var projection = d3.geo.mercator()
+  .scale((width + 1) / 2 / Math.PI)
+  .translate([width / 2, height / 2])
+  .precision(.1);
+  
+   var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
+  var path = d3.geo.path()
+  .projection(projection);
+
+  var graticule = d3.geo.graticule();
+  
+if(!svg)
+{
+  svg = d3.select("#canvas-map").append("svg")
+  .attr("width", width)
+  .attr("height", height);
+}
+
+  svg.append("path")
+  .datum(graticule)
+  .attr("class", "graticule")
+  .attr("d", path);
+  
+  //for the zoooming & paning
+   var g = svg.append("g");
+   
+   svg
+    .call(zoom)
+    .call(zoom.event);
+
+    function log10(val) {
+      return Math.log(val);
+    }
+
+  var quantize = d3.scale.quantize()
+  .domain([0, 1.0])
+  .range(d3.range(COLOR_COUNTS).map(function(i) { return i }));
+
+  quantize.domain(
+    [
+      d3.min(countries, function(d){
+       return (+d.data[0].D) 
+      }),
+      d3.max(countries, function(d){
+       return (+d.data[0].D) 
+      })
+    ]
+  );
+
+  //After this it's all about drawing the map (borders and so on)
+  d3.json("https://s3-us-west-2.amazonaws.com/vida-public/geo/world-topo-min.json", function(error, world) {
+    mapCountries = topojson.feature(world, world.objects.countries).features;
+
+    svg.append("path")
+    .datum(graticule)
+    .attr("class", "choropleth")
+    .attr("d", path);
+    
+    g.append("path")
+    .datum({type: "LineString", coordinates: [[-180, 0], [-90, 0], [0, 0], [90, 0], [180, 0]]})
+    .attr("class", "equator")
+    .attr("d", path);
+
+    var country = g.selectAll(".country").data(mapCountries);
+
+    country.enter().insert("path")
+    .attr("class", "country")
+    .attr("d", path)
+    .attr("id", function(d,i) { return d.id; })
+    .attr("title", function(d) { return d.properties.name; })
+    .style("fill", function(d) {
+      var matchedCountry = getCountryByName(countries, d.properties.name);
+
+      if(matchedCountry)
+      {
+        // var extreme = $.grep(extremes, function(e){ return e.Cause == "02"})[0];
+        // console.log("Extreme: " + extreme.D);
+        // var domain = generateColorScale(0, extreme.D+1 ,9);
+        // console.log("Domain: " + domain);
+        //make this domain dynamic
+        //var c = d3.scale.threshold().domain(domain).range(colors);
+        var c = d3.scale.threshold().domain(generateColorScale(0, 1 , COLOR_COUNTS)).range(colors);
+        var co =  c(matchedCountry.getColorCriteriumValue(colorCriterium)).getColors();
+        d.properties.color = co;
+
+        return "rgb("+co.r + ","+co.g+","+co.b+")";
+
+       } 
+       else {
+        return "#ccc";
+       }
+            
+    })
+
+  /* When the mouse is howered over a country */
+  .on("mousemove", function(d) {
+    var html = "";
+    var matchedCountry = getCountryByName(countries, d.properties.name);
+    /* Show the name */
+    html += "<div class=\"tooltip_kv\">";
+    html += "<span class=\"tooltip_key\">";
+    html += d.properties.name;
+    html += "</span>";
+    html += "<span class=\"tooltip_value\">";
+    html += matchedCountry ? matchedCountry.getCorrelation() : "";
+    html += "";
+    html += "</span>";
+    html += "</div>";
+
+    $("#tooltip-container").html(html);
+    $(this).attr("fill-opacity", "0.5");
+    $("#tooltip-container").show();
+
+    var coordinates = d3.mouse(this);
+
+    var map_width = $('.choropleth')[0].getBoundingClientRect().width;
+
+    if (d3.event.pageX < map_width / 2) {
+      d3.select("#tooltip-container")
+      .style("top", (d3.event.layerY + 15) + "px")
+      .style("left", (d3.event.layerX + 15) + "px")
+    } else {
+      var tooltip_width = $("#tooltip-container").width();
+      d3.select("#tooltip-container")
+      .style("top", (d3.event.layerY + 15) + "px")
+      .style("left", (d3.event.layerX - tooltip_width - 30) + "px")
+    }
+  })
+  .on("mouseout", function() {
+    $(this).attr("fill-opacity", "1.0");
+    $("#tooltip-container").hide();
+  })
+  //action when clicking with the mouse on a country
+  .on("click", function(d){
+    selectedCountry = getCountryByName(countries, d.properties.name);
+    changeCountrySelection();
+  
+
+  });
+
+  g.append("path")
+  .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+  .attr("class", "boundary")
+  .attr("d", path);
+
+  svg.attr("height", config.height * 2.2 / 3);
+  });
+  
+  //function that zooms and pan with limits (cannot go outside the map)
+   function zoomed() {
+	   var t = d3.event.translate;
+	   var s = d3.event.scale;
+	   
+	   var w_max = 0;
+	   var w_min = width * (1 - s);
+	   var h_max = height < s*width/1.25 ? s*(width/1.25-height)/1.25 : (1-s)*height/1.25;
+	   var h_min = height < s*width/1.25 ? -s*(width/1.25-height)/2-(s-1)*height : (1-s)*height/1.25;
+		
+		//mark the limits of the translate
+	   t[0] = Math.min(w_max, Math.max(w_min, t[0]));
+	   t[1] = Math.min(h_max, Math.max(h_min, t[1]));
+	   zoom.translate(t);
+
+	   g.attr("transform", "translate(" + t + ")scale(" + s + ")");
+}
+  d3.select(self.frameElement).style("height", (height * 2.3 / 3) + "px");
+}
---
+++ public/layout/app/components/map/mapScript.js
@@ -0,0 +1,160 @@
+function Country(name, data){
+      this.name = name;
+      this.data = data;
+      this.getColorCriteriumValue = function(criterium)
+      {
+        if(criterium == "correlation")
+        {
+          return this.getCorrelation();
+        }
+        else
+        {
+          var dataType = criterium.substring(0,1);
+          var number = criterium.substring(1,3);
+          console.log("number: " + number + " dataType: " + dataType);
+
+          var matchingData =  $.grep(this.data, function(d){ return d.Cause == number })[0];
+
+          console.log("Country: " + this.name + " Cause " + matchingData.Cause + " Death " +  matchingData.D/this.getTotalAmountOfDeaths()+ " Media " +  matchingData.M/this.getTotalAmountOfMedia());
+
+          if(dataType == "D")
+          {
+            return matchingData.D/this.getTotalAmountOfDeaths();
+          }else
+          {
+            return matchingData.M/this.getTotalAmountOfMedia();
+          }
+
+        }
+         
+      };
+      this.getTotalAmountOfDeaths = function(){
+
+        var total = 0;
+
+        $.each(this.data, function(index, value){
+
+          total += value.D;
+        });
+        return total;
+      };
+      this.getTotalAmountOfMedia = function(){
+
+        var total = 0;
+
+        $.each(this.data, function(index, value){
+
+          total += value.M;
+        });
+        return total;
+      };
+      this.getCorrelation = function(){
+        var correlation = 0;
+        var sum = 0;
+        var counter = 0;
+        $.each(this.data, function(index, value){
+          sum += Math.abs(value.D - value.M);
+          counter++;
+        });
+        correlation = sum / counter;
+        return Math.round(correlation); 
+      }
+    }
+
+
+   function getDataForCountry(data)
+   {
+    var countryData = [];
+    addDataToCountryDataArray(countryData, "01", parseInt(data.D01), parseInt(data.M01));
+    addDataToCountryDataArray(countryData, "02", parseInt(data.D02), parseInt(data.M02));
+    addDataToCountryDataArray(countryData, "03", parseInt(data.D03), parseInt(data.M03));
+    addDataToCountryDataArray(countryData, "04", parseInt(data.D04), parseInt(data.M04));
+    addDataToCountryDataArray(countryData, "05", parseInt(data.D05), parseInt(data.M05));
+    addDataToCountryDataArray(countryData, "06", parseInt(data.D06), parseInt(data.M06));
+    addDataToCountryDataArray(countryData, "07", parseInt(data.D07), parseInt(data.M07));
+   // countryData.getColorCriteriumValue("D01");
+
+    return new Country(data.Country, countryData);
+  }
+
+  function getDataForCountries(data)
+  {
+    var countries = [];
+    $.each(data, function(index, value){
+      countries.push(getDataForCountry(value));
+
+    });
+    return countries;
+  }
+
+  function getCountryByName(countries, name){
+    var foundCountries =  $.grep(countries, function(e){ return e.name == name});
+        return foundCountries[0]///>0?foundCountries[0]: false;
+  }
+
+
+  function addDataToCountryDataArray(array, cause, death, media)
+  {
+    var value = {Cause:cause, D:death, M:media};
+    array.push(value);
+
+
+    var extreme =  $.grep(extremes, function(e){ return e.Cause == cause});
+    if(extreme.length == 0)
+    {
+      extremes.push(value);
+    }
+    else
+    {
+      if(extreme.D < death)
+      {
+        extreme.D = death;
+      }
+      if(extreme.M < media)
+      {
+        extreme.M = media;
+      }
+    }
+  }
+
+  function generateColorScale(minimum, maximum, numberOfValues)
+  {
+    var result = Array(numberOfValues);
+    var gap = (maximum - minimum)/(numberOfValues-1);
+
+
+    var currentValue = minimum;
+    for(var i = 0; i<numberOfValues; i++)
+    {
+      result[i] = currentValue;
+      currentValue += gap;
+    }
+   // result[i+1] = currentValue;
+    return result;
+  }
+
+  //Current selected country on map
+  var selectedCountry;
+  //List of all countries
+  var countries = [];
+  //List of extremes for each Cause of death
+  var extremes = [];
+
+var CauseIndex = 3;
+
+function changeColorCodingMap()
+{
+  colorCriterium = "D0" + CauseIndex;
+//update();
+  drawMap();
+  CauseIndex ++;
+  if(CauseIndex >7)
+  {
+    CauseIndex = 1;
+  }
+
+
+  //change the domain
+
+  //change selected variable
+}
