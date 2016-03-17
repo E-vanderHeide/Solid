2750d45678deb09fca0959f7a2c0f6da0db865c2
ingridsorensen <iingridsorensen@gmail.com>
2016-03-17 15:37:01 +0100

    sunburst done

---
+++ public/app/components/treemap/sweden2012.js
@@ -0,0 +1,41 @@
+//Sweden 2012
+death2012_json = {
+ "name": "Sweden", "depth":0, "size": 83269, "coverage": 0,
+ "children":
+ [
+  {
+   "name": "Diseases", "depth":1, "size": 78588, "coverage": 80, 
+   "children":
+   [
+    {"name": "Infectious diseases", "depth":2, "size": 2210, "coverage": 50},
+    {"name": "Neoplasms", "depth":2, "size": 23103, "coverage": 0},
+    {"name": "Endocrine diseases", "depth":2, "size": 2425, "coverage": 20},
+    {"name": "Mental disorders", "depth":2, "size": 5828, "coverage": 0},  
+    {"name": "Circulatory diseases", "depth":2, "size": 34949, "coverage": 40},
+    {"name": "Respiratory diseases", "depth":2, "size": 6163, "coverage": 10},
+    {"name": "Digestive diseases", "depth":2, "size": 2818, "coverage": 80},
+    {"name": "Genitourinary diseases", "depth":2, "size": 1092, "coverage": 95}
+   ]
+  },
+  {
+    "name": "Accidents", "depth":1, "size": 1834, "coverage": 100,
+    "children":
+    [
+      {"name": "Transport accidents", "depth":2, "size": 333, "coverage": 0},
+      {"name": "Fall accidents", "depth":2, "size": 945, "coverage": 20},
+      {"name": "Drowning accidents", "depth":2, "size": 81, "coverage": 40},
+      {"name": "Fire accidents", "depth":2, "size": 70, "coverage": 70},
+      {"name": "Poisoning accidents", "depth":2, "size": 405, "coverage": 0} 
+    ]
+  },
+  {
+    "name": "Others", "depth":1, "size": 2847, "coverage": 50,
+    "children":
+    [
+      {"name": "Others", "depth":2, "size": 1631, "coverage": 50},
+      {"name": "Assault", "depth":2, "size": 62, "coverage": 50},
+      {"name": "Suicide", "depth":2, "size": 1154, "coverage": 60}
+    ]
+  }
+ ]
+}
